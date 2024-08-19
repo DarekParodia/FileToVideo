986ff6d648d4be2a27ea425d7a2a7fb88ed2c668
DarekParodia <darekparodia@gmail.com>
2024-08-19 20:56:21 +0200

    fixed std::setprecission bug

--- src/settings.h
+++ src/settings.h
@@ -4,7 +4,7 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 2
-#define VERSION_PATCH 0
+#define VERSION_PATCH 1
 
 #define HEADER_FRAMES 2 // WATNING!! don't change this value to ensure that header is 100% readable for the decoder
                         // reserving first 2 frames for header is huge waste of space but it is more reliable that way. default frames stores around 300KB (with pixel width of 1, header uses larger pixel width)of data while header needs around 37 bytes (overkill just to be sure)
--- src/utils/timer.h
+++ src/utils/timer.h
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <string>
 #include <sstream>
+#include <iomanip>
 
 class timer
 {
