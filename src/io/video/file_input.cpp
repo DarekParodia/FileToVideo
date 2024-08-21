4a5dd8a2ee0c4e03fc6c4b8cd39a846646a2e603
DarekParodia <darekparodia@gmail.com>
2024-08-21 18:38:19 +0200

    added file_input class

---
+++ src/io/video/file_input.cpp
@@ -0,0 +1,117 @@
+#include "file_input.h"
+
+namespace io::video
+{
+    FileInput::FileInput(std::string filename)
+    {
+        this->filename = filename;
+    }
+
+    FileInput::~FileInput()
+    {
+        close();
+    }
+
+    void FileInput::open()
+    {
+        // Open video file
+        if (avformat_open_input(&pFormatCtx, filename.c_str(), nullptr, nullptr) != 0)
+        {
+            logger.error("Could not open file: " + filename);
+            exit(1);
+        }
+
+        // Retrieve stream information
+        if (avformat_find_stream_info(pFormatCtx, nullptr) < 0)
+        {
+            logger.error("Could not find stream information");
+            exit(1);
+        }
+
+        // Find the first video stream
+        videoStreamIndex = -1;
+        for (unsigned int i = 0; i < pFormatCtx->nb_streams; i++)
+        {
+            if (pFormatCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+            {
+                videoStreamIndex = i;
+                break;
+            }
+        }
+
+        if (videoStreamIndex == -1)
+        {
+            logger.error("Could not find video stream");
+            exit(1);
+        }
+
+        // Get codec parameters
+        auto codecParameters = pFormatCtx->streams[videoStreamIndex]->codecpar;
+
+        // Find the decoder for the video stream
+        pCodec = const_cast<AVCodec *>(avcodec_find_decoder(codecParameters->codec_id));
+        if (pCodec == nullptr)
+        {
+            logger.error("Unsupported codec!");
+            exit(1);
+        }
+
+        // Allocate a codec context for the decoder
+        pCodecCtx = avcodec_alloc_context3(pCodec);
+        if (avcodec_parameters_to_context(pCodecCtx, codecParameters) < 0)
+        {
+            logger.error("Could not copy codec parameters to codec context");
+            exit(1);
+        }
+
+        // Open codec
+        if (avcodec_open2(pCodecCtx, pCodec, nullptr) < 0)
+        {
+            logger.error("Could not open codec");
+            exit(1);
+        }
+
+        // Allocate video frame
+        pFrameRGB = av_frame_alloc();
+        if (pFrameRGB == nullptr)
+        {
+            logger.error("Could not allocate video frame");
+            exit(1);
+        }
+
+        numBytes = av_image_get_buffer_size(AV_PIX_FMT_RGB24, pCodecCtx->width, pCodecCtx->height, 1);
+        buffer = (uint8_t *)av_malloc(numBytes * sizeof(uint8_t));
+        av_image_fill_arrays(pFrameRGB->data, pFrameRGB->linesize, buffer, AV_PIX_FMT_RGB24, pCodecCtx->width, pCodecCtx->height, 1);
+
+        // Initialize SWS context for software scaling
+        sws_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, nullptr, nullptr, nullptr);
+
+        if (sws_ctx == nullptr)
+        {
+            logger.error("Could not initialize the conversion context");
+            exit(1);
+        }
+
+        logger.debug("Opened video file: " + filename);
+    }
+
+    void FileInput::close()
+    {
+        // Cleanup
+        av_frame_free(&pFrame);
+        av_frame_free(&pFrameRGB);
+        avcodec_free_context(&pCodecCtx);
+        avformat_close_input(&pFormatCtx);
+        avformat_free_context(pFormatCtx);
+        av_freep(&buffer);
+        sws_freeContext(sws_ctx);
+
+        logger.debug("Closed video file: " + filename);
+    }
+
+    uint8_t *FileInput::readFrame(size_t &size)
+    {
+        uint8_t *frame = nullptr;
+        return frame;
+    }
+}
---
+++ src/io/video/file_input.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "video_input.h"
+
+extern "C"
+{
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
+#include <libavutil/imgutils.h>
+#include <libswscale/swscale.h>
+}
+
+#include <cstdio>
+#include <string>
+
+#include "utils/logger.h"
+
+namespace io::video
+{
+    class FileInput : public VideoInput
+    {
+    public:
+        FileInput(std::string filename);
+        ~FileInput();
+        void open() override;
+        void close() override;
+        uint8_t *readFrame(size_t &size);
+
+    private:
+        std::string filename;
+        AVFormatContext *pFormatCtx = nullptr;
+        AVCodecContext *pCodecCtx = nullptr;
+        AVFrame *pFrame = nullptr;
+        AVFrame *pFrameRGB = nullptr;
+        AVPacket packet;
+        int videoStreamIndex = -1;
+        int numBytes = 0;
+        AVCodec *pCodec = nullptr;
+        uint8_t *buffer = nullptr;
+        struct SwsContext *sws_ctx = nullptr;
+    };
+}
